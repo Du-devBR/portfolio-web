7adc6b2a9def53870f94367ee419cdd81cdac0c1
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-01-02 18:08:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portifolio eduardo ananias
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <>
+      <h1 className="text-zinc-800">Hello word eduardo</h1>
+      <Button>Apertar</Button>
+    </>
+  );
+}
+
+export default App;
