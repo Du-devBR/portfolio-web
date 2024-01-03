e3a7e64cf1cade53f636b100aadde1b7c41f42de
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-01-03 19:17:34 -0300

    feat: criado componente gridContainer

---
+++ src/pages/_components/grid-container.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface GrindContainerProps {
+  children: ReactNode;
+}
+
+export function GridContainer({ children }: GrindContainerProps) {
+  return (
+    <div className="max-w-5xl m-auto py-8 max-sm:px-6 max-md:px-8 max-lg:px-16 max-xl:px-12">
+      {children}
+    </div>
+  );
+}
