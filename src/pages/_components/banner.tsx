13d22ee7877360d244c4679f6382d609a88711eb
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-01-04 08:37:03 -0300

    feat: adicionado botoes ao info

--- src/pages/_components/banner.tsx
+++ src/pages/_components/banner.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { technologyIcons } from "./constants/technology-icon-data";
 
 export function Banner() {
@@ -19,6 +20,15 @@ export function Banner() {
               {item.technology}
             </div>
           ))}
+        </div>
+        <div className="flex gap-4 mt-4">
+          <Button
+            type="button"
+            className="bg-zinc-50 text-zinc-900 border-zinc-900 hover:bg-zinc-200 dark:bg-zinc-800 dark:border-zinc-50 border-solid border-2 dark:text-zinc-50  dark:hover:bg-zinc-900"
+          >
+            <a href="#">Veja meus projetos</a>
+          </Button>
+          <Button type="button">Baixar Curriculo</Button>
         </div>
       </div>
       <div className="w-full h-2 bg-red-300"></div>
