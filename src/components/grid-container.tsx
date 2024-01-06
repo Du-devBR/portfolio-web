12b40d23906a4275b223784866e7017029e51d87
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-01-06 16:59:34 -0300

    style: modificado largura do container para telas até 1280

--- src/components/grid-container.tsx
+++ src/components/grid-container.tsx
@@ -6,7 +6,7 @@ interface GrindContainerProps {
 
 export function GridContainer({ children }: GrindContainerProps) {
   return (
-    <div className="max-w-5xl m-auto py-8 max-sm:px-6 max-md:px-8 max-lg:px-16 max-xl:px-12">
+    <div className="max-w-5xl m-auto py-8 max-sm:px-6 max-md:px-8 max-lg:px-8 max-xl:px-12">
       {children}
     </div>
   );
