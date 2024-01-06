69da7ac9e8b380e09343e2b45d030b6b552890a6
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-01-06 17:24:27 -0300

    feat: criado componente footer e add a home

---
+++ src/pages/_components/footer.tsx
@@ -0,0 +1,21 @@
+import logo from "@/assets/logo.svg";
+
+export function Footer() {
+  return (
+    <footer className="flex flex-col mt-40 max-md:mt-24 bg-zinc-200 dark:bg-zinc-800  absolute top-auto w-full left-0 text-center py-8 border-b-primary border-b-4">
+      <div className="flex w-full items-center justify-center gap-2 ">
+        <img src={logo} alt="" className="w-8 max-sm:w-4" />
+        <span className="max-sm:text-sm">{new Date().getFullYear()} - </span>
+        <p className="max-sm:text-sm">
+          Eduardo Ananias - Design by{" "}
+          <a
+            href="https://www.linkedin.com/in/eduardo-ananias-29a53048/"
+            className="text-primary "
+          >
+            Eduardo Ananias
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import { GridContainer } from "../components/grid-container";
 import { About } from "./_components/about";
 import { Banner } from "./_components/banner";
 import { Contact } from "./_components/contact";
+import { Footer } from "./_components/footer";
 import { Header } from "./_components/header";
 import { Projects } from "./_components/projects";
 import { Skills } from "./_components/skills";
@@ -15,6 +16,7 @@ export function Home() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </GridContainer>
   );
 }
