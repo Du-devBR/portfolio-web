aab500108cd6c1d08f4536f7767cd0db631fef0a
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-01-09 19:33:27 -0300

    feat: adicionado texto para sessao sobre

--- src/pages/_components/about.tsx
+++ src/pages/_components/about.tsx
@@ -1,3 +1,4 @@
+import { CalculateAge } from "@/lib/calculate-age";
 import { contactIconData } from "./constants/contact-icon-data";
 
 export function About() {
@@ -33,11 +34,16 @@ export function About() {
             Conheça um pouco sobre mim
           </h3>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries.
+            {`Eu sou Eduardo e tenho ${CalculateAge(
+              new Date(1992, 10, 19)
+            )} anos e sou apaixonado por tecnologia e inovação. Sou formado em Engenharia de Computadores pela Universidade Cidade de São Paulo,
+            com especialização em desenvolvimento de softwares. Ao longo dos meus 9 anos de experiência na área de telecomunicações, desenvolvi
+            habilidades cruciais para atender, compreender e solucionar problemas e desafios. Minha dedicação à qualidade, agilidade e
+            comprometimento com os clientes sempre foi a minha prioridade.Recentemente, fiz a migração para a área de Tecnologia da Informação (TI)
+            com o objetivo de auxiliar pessoas a transformar ideias em negócios e projetos em soluções. Acredito que a tecnologia é uma ferramenta
+            poderosa para impulsionar o crescimento e a inovação em diversas áreas.lém disso, minha paixão pela tecnologia se estende à minha
+            vida pessoal, onde busco integrar a inovação no meu cotidiano. Valorizo a comunicação eficaz e prezo pela importância da família em minha
+            jornada. Sou movido pela busca constante de conhecimento e pela vontade de contribuir para o sucesso e prosperidade daqueles ao meu redor. `}
           </p>
         </div>
       </div>
