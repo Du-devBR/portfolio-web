715c4b69ee1c11229c7009feedba6567142bafb6
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-01-20 18:31:08 -0300

    feat: criado view more para about

--- src/pages/_components/about.tsx
+++ src/pages/_components/about.tsx
@@ -1,7 +1,10 @@
-import { CalculateAge } from "@/lib/calculate-age";
+import { aboutData } from "./constants/about-data";
 import { contactIconData } from "./constants/contact-icon-data";
+import { useState } from "react";
 
 export function About() {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <section id="about" className="flex flex-col mt-40 max-md:mt-24">
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 max-sm:text-2xl">
@@ -29,22 +32,22 @@ export function About() {
             ))}
           </ul>
         </div>
-        <div className="flex flex-col max-sm:text-center">
+        <div className="flex flex-col ">
           <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight max-sm:text-xl">
             Conheça um pouco sobre mim
           </h3>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
-            {`Eu sou Eduardo e tenho ${CalculateAge(
-              new Date(1992, 10, 19)
-            )} anos e sou apaixonado por tecnologia e inovação. Sou formado em Engenharia de Computadores pela Universidade Cidade de São Paulo,
-            com especialização em desenvolvimento de softwares. Ao longo dos meus 9 anos de experiência na área de telecomunicações, desenvolvi
-            habilidades cruciais para atender, compreender e solucionar problemas e desafios. Minha dedicação à qualidade, agilidade e
-            comprometimento com os clientes sempre foi a minha prioridade.Recentemente, fiz a migração para a área de Tecnologia da Informação (TI)
-            com o objetivo de auxiliar pessoas a transformar ideias em negócios e projetos em soluções. Acredito que a tecnologia é uma ferramenta
-            poderosa para impulsionar o crescimento e a inovação em diversas áreas.lém disso, minha paixão pela tecnologia se estende à minha
-            vida pessoal, onde busco integrar a inovação no meu cotidiano. Valorizo a comunicação eficaz e prezo pela importância da família em minha
-            jornada. Sou movido pela busca constante de conhecimento e pela vontade de contribuir para o sucesso e prosperidade daqueles ao meu redor. `}
+            {" "}
+            {showMore
+              ? aboutData.text
+              : `${aboutData.text.substring(0, 250)}...`}
           </p>
+          <button
+            className="self-end text-primary"
+            onClick={() => setShowMore(!showMore)}
+          >
+            {showMore ? "ver menos" : "ver mais"}
+          </button>
         </div>
       </div>
     </section>
